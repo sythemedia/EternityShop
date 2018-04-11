858cfdff8865fe0c1fef1dcec34ff39248af5f09
sythemedia <36555749+sythemedia@users.noreply.github.com>
2018-04-11 00:00:41 -0400

    Update config.php

--- config.php
+++ config.php
@@ -6,10 +6,10 @@ if (!defined('IN_ETERNTIY_SHOP'))
 }
 
 // Edit your settings below this line.  Default settings are used.  If you update these, make sure you do not commit these lines or you could cause problems for other users.
-define('DB_HOST', '???');
-define('DB_USER', '???');
-define('DB_PASS', '???');
-define('DB_NAME', '???');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'id5339757_test');
+define('DB_PASS', 'test123');
+define('DB_NAME', 'id5339757_test');
 define('DB_PORT', NULL); // Change from NULL if you need a non-default port
 define('DB_SOCK', NULL); // Change from NULL if you need a socket
 // Don't edit beyond this line unless you really know what you're doing!
